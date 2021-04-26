d7bfd72e86791eef442e2421eb7af51853456ac8
Prabhakar Singh <pksingh15007@gmail.com>
2021-04-26 13:39:50 +0530

    Update Pet.java
    Creation of Pet Entity class

--- PetPeers/src/main/java/com/hcl/cs/model/Pet.java
+++ PetPeers/src/main/java/com/hcl/cs/model/Pet.java
@@ -1,5 +1,74 @@
 package com.hcl.cs.model;
 
-public class Pet {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="PET")
+public class Pet {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="PETID")
+	private long petId;
+	
+	@Column(name="PETNAME")
+	private String petName;
+	
+	@Column(name="PETAGE")
+	private int petAge;
+	
+	@Column(name="PETPLACE")
+	private String petPlace;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="PETOWNERID")
+    private User user;
+
+	public long getPetId() {
+		return petId;
+	}
+
+	public void setPetId(long petId) {
+		this.petId = petId;
+	}
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+
+	public int getPetAge() {
+		return petAge;
+	}
+
+	public void setPetAge(int petAge) {
+		this.petAge = petAge;
+	}
+
+	public String getPetPlace() {
+		return petPlace;
+	}
+
+	public void setPetPlace(String petPlace) {
+		this.petPlace = petPlace;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}	
 }
