1ed425ccff0ea1a938539b1393acb6c074bd909e
anurashmitha <43899696+anurashmitha@users.noreply.github.com>
2021-04-26 14:23:38 +0530

    User Pojo class
    After modifications

--- PetPeers/src/main/java/com/hcl/cs/model/User.java
+++ PetPeers/src/main/java/com/hcl/cs/model/User.java
@@ -2,64 +2,77 @@ package com.hcl.cs.model;
 
 
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
-import org.hibernate.mapping.Set;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="USERID")
 	private long userId;
+	
 	@NotEmpty
+	@Column(name="USERNAME")
 	private String userName;
 	@NotEmpty
+	@Column(name="PASSWORD")
 	private String userPassword;
 	@NotEmpty
+	@Transient
 	private String confirmPassword;
-	private Set pets;
-	public User(long userId, String userName, String userPassword, String confirmPassword, Set pets) {
-		super();
-		this.userId = userId;
-		this.userName = userName;
-		this.userPassword = userPassword;
-		this.confirmPassword = confirmPassword;
-		this.pets = pets;
-	}
-	public User() {
-		super();
-	}
+	
+	@OneToMany(mappedBy="user")
+	private Set<Pet> pets = new HashSet<Pet>();
+
 	public long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(long userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getUserPassword() {
 		return userPassword;
 	}
+
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
 	public String getConfirmPassword() {
 		return confirmPassword;
 	}
+
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
 	}
-	public Set getPets() {
+
+	public Set<Pet> getPets() {
 		return pets;
 	}
-	public void setPets(Set pets) {
+
+	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
 	}
+	
+	
 
 }
