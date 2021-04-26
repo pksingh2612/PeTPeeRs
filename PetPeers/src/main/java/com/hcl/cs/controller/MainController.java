3c63efca378b0b0f162d044ef2a61da83e19d57f
Prabhakar Singh <pksingh15007@gmail.com>
2021-04-26 14:47:37 +0530

    update the Controller.java file
    Make three request handler
    1.  myPets
    2. SavePet
    3. buyPet

--- PetPeers/src/main/java/com/hcl/cs/controller/MainController.java
+++ PetPeers/src/main/java/com/hcl/cs/controller/MainController.java
@@ -1,5 +1,31 @@
 package com.hcl.cs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MainController {
 
+	//myPets(HttpServletRequest request)
+	
+	@RequestMapping(value="/myPets",method=RequestMethod.GET)
+	public String myPets() {
+		return "myPetsPage";
+	}
+	
+	//savePet(@ModelAttribute("pet") Pet pet)
+	
+	@RequestMapping(value="/savePet",method=RequestMethod.GET)
+	public String savePet() {
+		return "homePage.jsp";
+	}
+	 
+	//buyPet(HttpServletRequest request)
+	
+	@RequestMapping(value="/buyPet",method=RequestMethod.GET)
+	public String buyPet() {
+		return "redirect:/myPets";
+	}
 }
