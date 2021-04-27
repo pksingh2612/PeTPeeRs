2ae04e9ba612ca350ab71c61bd3b8357ae2e880e
anurashmitha <43899696+anurashmitha@users.noreply.github.com>
2021-04-27 09:53:08 +0530

    MainController
    Methods for
    1.saveUser
    2.authenticateUser
    3.home
    4.logout

--- PetPeers/src/main/java/com/hcl/cs/controller/MainController.java
+++ PetPeers/src/main/java/com/hcl/cs/controller/MainController.java
@@ -1,5 +1,50 @@
 package com.hcl.cs.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hcl.cs.model.User;
+
 public class MainController {
+	Logger logger=Logger.getLogger(MainController.class);
+	
+	@Autowired
+	private User user;
+	
+	@RequestMapping(value="/saveUser")
+	public String saveUser(@ModelAttribute("user") User user){
+		if(){
+			return "SuccessloginPage";
+		}
+		else {
+			return "registrationPage";
+		}
+		
+	}
+	
+	@RequestMapping(value="/authenticateUser")
+	public String authenticateUser(HttpServletRequest request, @ModelAttribute("user") User user) {
+		if() {
+			return "SuccesshomePage";
+		}
+		else {
+			return "loginPage";
+		}
+	}
+	
+	@RequestMapping(value="/home")
+	public String home() {
+		return "homePage";
+	}
+	
+	@RequestMapping(value="/logout")
+	public String logout(HttpServletRequest request) {
+		return "loginPage";
+	}
+	
 
 }
